7a2d1d5b2f389882e59b81b75fe0593c3a60b4a2
Papa Neto <papaneto@hotmail.com>
2018-03-26 14:05:50 -0300

    fim layout do componente de GIF

--- src/app/components/gif-item/gif-item.component.ts
+++ src/app/components/gif-item/gif-item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Gif } from '../../model/gif/gif.model';
+
+declare var $: any;
 
 @Component({
   selector: 'app-gif-item',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GifItemComponent implements OnInit {
 
+  private gif: Gif;
+
   constructor() { }
 
   ngOnInit() {
+    this.gif = new Gif();
+  }
+
+  private showGifInfo(e: any): void {
+    e.target.childNodes[3].classList.add('hovered');
+  }
+
+  private hideGifInfo(e: any): void {
+    e.target.childNodes[3].classList.remove('hovered');
   }
 
 }
